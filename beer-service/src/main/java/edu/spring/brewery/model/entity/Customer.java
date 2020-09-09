795836718969052d12ea8dc993f1f9044baeca24
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-09-09 08:17:07 +0530

    Adding customer entity

---
+++ beer-service/src/main/java/edu/spring/brewery/model/entity/Customer.java
@@ -0,0 +1,33 @@
+package edu.spring.brewery.model.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Customer {
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	private UUID id;
+	
+	private String name;
+
+	private String email;
+
+}
